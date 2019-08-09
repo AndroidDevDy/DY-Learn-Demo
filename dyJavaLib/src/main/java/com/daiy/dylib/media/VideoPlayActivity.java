9b9a6653ad2583f561090b58db559c0f46a52c13
daiyue <dev_daiyue@163.com>
2019-08-09 17:46:31 +0800

    add test model

---
+++ dyJavaLib/src/main/java/com/daiy/dylib/media/VideoPlayActivity.java
@@ -0,0 +1,17 @@
+package com.daiy.dylib.media;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.daiy.dylib.R;
+import com.daiy.dylib.activity.BaseActivity;
+
+public class VideoPlayActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_play);
+
+    }
+}
